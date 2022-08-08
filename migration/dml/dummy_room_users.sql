aac3d67a76a3115af490ef3efe58934cbe9ad64f
中田誠人 <masato.nakata1250softbank.com@gmail.com>
2022-08-08 15:53:30 +0900

    feat: ルームユーザーのダミーsql文作成

---
+++ migration/dml/dummy_room_users.sql
@@ -0,0 +1,66 @@
+INSERT INTO room_users
+VALUES (
+        1,
+        1,
+        1,
+        0,
+        default,
+        default,
+        default
+    ), (
+        2,
+        2,
+        1,
+        0,
+        default,
+        default,
+        default
+    ), (
+        3,
+        1,
+        2,
+        0,
+        default,
+        default,
+        default
+    ), (
+        4,
+        3,
+        2,
+        0,
+        default,
+        default,
+        default
+    ), (
+        5,
+        1,
+        3,
+        0,
+        default,
+        default,
+        default
+    ), (
+        6,
+        4,
+        3,
+        0,
+        default,
+        default,
+        default
+    ), (
+        7,
+        1,
+        4,
+        1,
+        default,
+        default,
+        default
+    ), (
+        8,
+        5,
+        4,
+        0,
+        default,
+        default,
+        default
+    )
