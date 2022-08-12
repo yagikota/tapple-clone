867c16dd742fd9ef7a173d1e12e1b05b8cfefb75
中田誠人 <masato.nakata1250softbank.com@gmail.com>
2022-08-13 01:46:04 +0900

    fix: API定義の修正による変更

--- pkg/usecase/model/user.go
+++ pkg/usecase/model/user.go
@@ -42,6 +42,8 @@ type Room struct {
 	ID            RoomID       `json:"id"`
 	Unread        int          `json:"unread,omitempty"`
 	IsPinned      bool         `json:"is_pinned,omitempty"`
+	Name          string       `json:"name,omitempty"`
+	Icon          string       `json:"icon,omitempty"`
 	LatestMessage *Message     `json:"latest_message,omitempty"`
 	Users         UserSlice    `json:"users,omitempty"`
 	Messages      MessageSlice `json:"messages,omitempty"`
@@ -51,23 +53,28 @@ func RoomFromEntity(entity *entity.Room) *Room {
 	r := &Room{
 		ID: RoomID(entity.ID),
 	}
-	if entity.R != nil {
-		if len(entity.R.RoomUsers) != 0 {
-			r.IsPinned = entity.R.RoomUsers[0].IsPinned
-		}
-		if len(entity.R.Messages) != 0 {
-			r.LatestMessage = MessageFromEntity(entity.R.Messages[0])
-		}
-		if len(entity.R.RoomUsers) != 0 {
-			if entity.R.RoomUsers != nil {
-				uSlice := make(UserSlice, 0, len(entity.R.RoomUsers))
-				for i := range entity.R.RoomUsers {
-					uSlice = append(uSlice, UserFromEntity(entity.R.RoomUsers[i].R.User))
-				}
-				r.Users = uSlice
-			}
-		}
-	}
+	r.IsPinned = entity.R.RoomUsers[0].IsPinned
+	r.Name = UserFromEntity(entity.R.RoomUsers[0].R.User).Name
+	r.Icon = UserFromEntity(entity.R.RoomUsers[0].R.User).Icon
+	r.LatestMessage = MessageFromEntity(entity.R.Messages[0])
+
+	// if entity.R != nil {
+	// 	if len(entity.R.RoomUsers) != 0 {
+	// 		r.IsPinned = entity.R.RoomUsers[0].IsPinned
+	// 	}
+	// 	if len(entity.R.Messages) != 0 {
+	// 		r.LatestMessage = MessageFromEntity(entity.R.Messages[0])
+	// 	}
+	// 	if len(entity.R.RoomUsers) != 0 {
+	// 		if entity.R.RoomUsers != nil {
+	// 			uSlice := make(UserSlice, 0, len(entity.R.RoomUsers))
+	// 			for i := range entity.R.RoomUsers {
+	// 				uSlice = append(uSlice, UserFromEntity(entity.R.RoomUsers[i].R.User))
+	// 			}
+	// 			r.Users = uSlice
+	// 		}
+	// 	}
+	// }
 
 	return r
 }
