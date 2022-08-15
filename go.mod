f5738f7283254d80d923cef8be9e4125187f4eb7
yagikota <mdryzk64smsh@gmail.com>
2022-08-15 15:17:14 +0900

    fix: findUserByUserIDのテスト修正

--- go.mod
+++ go.mod
@@ -5,15 +5,16 @@ go 1.18
 require (
 	github.com/friendsofgo/errors v0.9.2
 	github.com/gin-gonic/gin v1.8.1
+	github.com/go-playground/assert/v2 v2.0.1
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/golang/mock v1.6.0
-	github.com/google/go-cmp v0.5.6
 	github.com/volatiletech/null/v8 v8.1.2
 	github.com/volatiletech/sqlboiler/v4 v4.12.0
 	github.com/volatiletech/strmangle v0.0.4
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
@@ -26,7 +27,10 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/cast v1.4.1 // indirect
+	github.com/stretchr/objx v0.4.0 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	github.com/volatiletech/inflect v0.0.1 // indirect
 	github.com/volatiletech/randomize v0.0.1 // indirect
@@ -37,4 +41,5 @@ require (
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/adaptor/http/user_handler_test.go
+++ pkg/adaptor/http/user_handler_test.go
@@ -2,42 +2,39 @@ package http
 
 import (
 	"context"
-	"log"
 	"net/http"
-	"reflect"
+	"net/http/httptest"
 	"testing"
+	"time"
 
 	mock "github.com/CyberAgentHack/2208-ace-go-server/pkg/mock/usecase"
-	"github.com/CyberAgentHack/2208-ace-go-server/pkg/usecase"
 	"github.com/CyberAgentHack/2208-ace-go-server/pkg/usecase/model"
 	"github.com/gin-gonic/gin"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestNewUserHandler(t *testing.T) {
-	type args struct {
-		uu usecase.IUserUsecase
-	}
-	tests := []struct {
-		name string
-		args args
-		want *userHandler
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := NewUserHandler(tt.args.uu); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewUserHandler() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
+// func TestNewUserHandler(t *testing.T) {
+// 	type args struct {
+// 		uu usecase.IUserUsecase
+// 	}
+// 	tests := []struct {
+// 		name string
+// 		args args
+// 		want *userHandler
+// 	}{
+// 		// TODO: Add test cases.
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			if got := NewUserHandler(tt.args.uu); !reflect.DeepEqual(got, tt.want) {
+// 				t.Errorf("NewUserHandler() = %v, want %v", got, tt.want)
+// 			}
+// 		})
+// 	}
+// }
 
 func Test_userHandler_findUserByUserID_200(t *testing.T) {
-	gin.SetMode(gin.TestMode)
-
 	type args struct {
 		ctx    context.Context
 		userID int
@@ -55,7 +52,15 @@ func Test_userHandler_findUserByUserID_200(t *testing.T) {
 					ctx:    &gin.Context{},
 					userID: 1,
 				}
-				m.EXPECT().FindUserByUserID(args.ctx, args.userID).Return(&model.User{}, nil)
+				// gomock.Any()にしないとエラー
+				m.EXPECT().FindUserByUserID(gomock.Any(), args.userID).Return(&model.User{
+					ID:       1,
+					Name:     "name1",
+					Icon:     "/icon1",
+					Gender:   1,
+					BirthDay: time.Date(2022, 1, 1, 0, 0, 0, 0, time.Local),
+					Location: 1,
+				}, nil)
 			},
 			want: func(c *gin.Context) {
 				c.JSON(http.StatusOK, &model.User{})
@@ -64,112 +69,135 @@ func Test_userHandler_findUserByUserID_200(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			gin.SetMode(gin.TestMode)
+			// mock登録
 			controller := gomock.NewController(t)
 			defer controller.Finish()
 			mock := mock.NewMockIUserUsecase(controller)
-			// NewUserHandler(mock)
 			tt.prepareMockFn(mock)
-			uh := &userHandler{
-				uUsecase: mock,
-			}
+			// user handler 初期化
+			uh := NewUserHandler(mock)
 
-			if got := uh.findUserByUserID(); cmp.Diff(got, tt.want) == "" {
-				log.Println(cmp.Diff(got, tt.want))
-				t.Errorf("userHandler.findUserByUserID() = %v, want %v", got, tt.want)
-			}
+			// エンドポイントを登録
+			// router := InitRouter()
+			router := gin.Default()
+			// ハンドラー登録(mockが登録させたハンドラー)
+			router.GET(usersAPIRoot+"/:user_id", func(c *gin.Context) { c.Set("user_id", 1) }, uh.findUserByUserID())
+
+			// レスポンスを受け止める*httptest.ResponseRecorder
+			rec := httptest.NewRecorder()
+			// テストで送信するリクエスト
+			req := httptest.NewRequest(http.MethodGet, usersAPIRoot+"/1", nil)
+			// リクエスト送信
+			router.ServeHTTP(rec, req)
+
+			// 結果確認
+			assert.Equal(t, http.StatusOK, rec.Code)
+			assert.JSONEq(
+				t,
+				`{
+					"id":1,
+					"name":"name1",
+					"icon":"/icon1",
+					"gender":1,
+					"birthday":"2022-01-01T00:00:00+09:00",
+					"location":1
+				}`,
+				rec.Body.String(),
+			)
 		})
 	}
 }
 
-func Test_userHandler_findUsers(t *testing.T) {
-	type fields struct {
-		uUsecase usecase.IUserUsecase
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   gin.HandlerFunc
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			uh := &userHandler{
-				uUsecase: tt.fields.uUsecase,
-			}
-			if got := uh.findUsers(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("userHandler.findUsers() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
+// func Test_userHandler_findUsers(t *testing.T) {
+// 	type fields struct {
+// 		uUsecase usecase.IUserUsecase
+// 	}
+// 	tests := []struct {
+// 		name   string
+// 		fields fields
+// 		want   gin.HandlerFunc
+// 	}{
+// 		// TODO: Add test cases.
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			uh := &userHandler{
+// 				uUsecase: tt.fields.uUsecase,
+// 			}
+// 			if got := uh.findUsers(); !reflect.DeepEqual(got, tt.want) {
+// 				t.Errorf("userHandler.findUsers() = %v, want %v", got, tt.want)
+// 			}
+// 		})
+// 	}
+// }
 
-func Test_userHandler_findRooms(t *testing.T) {
-	type fields struct {
-		uUsecase usecase.IUserUsecase
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		// buildStubs func(store *mock.Mo)
-		want gin.HandlerFunc
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			uh := &userHandler{
-				uUsecase: tt.fields.uUsecase,
-			}
-			if got := uh.findRooms(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("userHandler.findRooms() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
+// func Test_userHandler_findRooms(t *testing.T) {
+// 	type fields struct {
+// 		uUsecase usecase.IUserUsecase
+// 	}
+// 	tests := []struct {
+// 		name   string
+// 		fields fields
+// 		// buildStubs func(store *mock.Mo)
+// 		want gin.HandlerFunc
+// 	}{
+// 		// TODO: Add test cases.
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			uh := &userHandler{
+// 				uUsecase: tt.fields.uUsecase,
+// 			}
+// 			if got := uh.findRooms(); !reflect.DeepEqual(got, tt.want) {
+// 				t.Errorf("userHandler.findRooms() = %v, want %v", got, tt.want)
+// 			}
+// 		})
+// 	}
+// }
 
-func Test_userHandler_findRoomDetailByRoomID(t *testing.T) {
-	type fields struct {
-		uUsecase usecase.IUserUsecase
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   gin.HandlerFunc
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			uh := &userHandler{
-				uUsecase: tt.fields.uUsecase,
-			}
-			if got := uh.findRoomDetailByRoomID(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("userHandler.findRoomDetailByRoomID() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
+// func Test_userHandler_findRoomDetailByRoomID(t *testing.T) {
+// 	type fields struct {
+// 		uUsecase usecase.IUserUsecase
+// 	}
+// 	tests := []struct {
+// 		name   string
+// 		fields fields
+// 		want   gin.HandlerFunc
+// 	}{
+// 		// TODO: Add test cases.
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			uh := &userHandler{
+// 				uUsecase: tt.fields.uUsecase,
+// 			}
+// 			if got := uh.findRoomDetailByRoomID(); !reflect.DeepEqual(got, tt.want) {
+// 				t.Errorf("userHandler.findRoomDetailByRoomID() = %v, want %v", got, tt.want)
+// 			}
+// 		})
+// 	}
+// }
 
-func Test_userHandler_sendMessage(t *testing.T) {
-	type fields struct {
-		uUsecase usecase.IUserUsecase
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   gin.HandlerFunc
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			uh := &userHandler{
-				uUsecase: tt.fields.uUsecase,
-			}
-			if got := uh.sendMessage(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("userHandler.sendMessage() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
+// func Test_userHandler_sendMessage(t *testing.T) {
+// 	type fields struct {
+// 		uUsecase usecase.IUserUsecase
+// 	}
+// 	tests := []struct {
+// 		name   string
+// 		fields fields
+// 		want   gin.HandlerFunc
+// 	}{
+// 		// TODO: Add test cases.
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			uh := &userHandler{
+// 				uUsecase: tt.fields.uUsecase,
+// 			}
+// 			if got := uh.sendMessage(); !reflect.DeepEqual(got, tt.want) {
+// 				t.Errorf("userHandler.sendMessage() = %v, want %v", got, tt.want)
+// 			}
+// 		})
+// 	}
+// }
