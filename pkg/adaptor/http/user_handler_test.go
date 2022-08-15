8cc7b1d15077a70c86847a239db29a0e8acb9b27
yagikota <mdryzk64smsh@gmail.com>
2022-08-15 22:14:54 +0900

    fix: findUserByUserIDのテスト修正(testify suite利用)

--- pkg/adaptor/http/user_handler_test.go
+++ pkg/adaptor/http/user_handler_test.go
@@ -1,7 +1,6 @@
 package http
 
 import (
-	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -11,102 +10,69 @@ import (
 	"github.com/CyberAgentHack/2208-ace-go-server/pkg/usecase/model"
 	"github.com/gin-gonic/gin"
 	"github.com/golang/mock/gomock"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-// func TestNewUserHandler(t *testing.T) {
-// 	type args struct {
-// 		uu usecase.IUserUsecase
-// 	}
-// 	tests := []struct {
-// 		name string
-// 		args args
-// 		want *userHandler
-// 	}{
-// 		// TODO: Add test cases.
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			if got := NewUserHandler(tt.args.uu); !reflect.DeepEqual(got, tt.want) {
-// 				t.Errorf("NewUserHandler() = %v, want %v", got, tt.want)
-// 			}
-// 		})
-// 	}
-// }
+type UserHandlerTestSuite struct {
+	suite.Suite
+	router  *gin.Engine
+	mock    *mock.MockIUserUsecase
+	handler *userHandler
+}
 
-func Test_userHandler_findUserByUserID_200(t *testing.T) {
-	type args struct {
-		ctx    context.Context
-		userID int
-	}
+// https://stackoverflow.com/questions/50200933/difference-between-setupsuite-setuptest-in-testify-suites
+// The SetupSuite method will be run by testify once, at the very
+// start of the testing suite, before any tests are run.
+func (suite *UserHandlerTestSuite) SetupSuite() {
+	gin.SetMode(gin.TestMode)
 
-	tests := []struct {
-		name          string
-		prepareMockFn func(m *mock.MockIUserUsecase)
-		want          gin.HandlerFunc
-	}{
-		{
-			name: "findUserByUserID success response",
-			prepareMockFn: func(m *mock.MockIUserUsecase) {
-				args := args{
-					ctx:    &gin.Context{},
-					userID: 1,
-				}
-				// gomock.Any()にしないとエラー
-				m.EXPECT().FindUserByUserID(gomock.Any(), args.userID).Return(&model.User{
-					ID:       1,
-					Name:     "name1",
-					Icon:     "/icon1",
-					Gender:   1,
-					BirthDay: time.Date(2022, 1, 1, 0, 0, 0, 0, time.Local),
-					Location: 1,
-				}, nil)
-			},
-			want: func(c *gin.Context) {
-				c.JSON(http.StatusOK, &model.User{})
-			},
+	suite.router = gin.Default()
+	mockCtl := gomock.NewController(suite.T())
+	defer mockCtl.Finish()
+	suite.mock = mock.NewMockIUserUsecase(mockCtl)
+	suite.handler = NewUserHandler(suite.mock)
+
+	// ハンドラー登録(mockが登録させたハンドラー)
+	suite.router.GET(usersAPIRoot+"/:user_id", func(c *gin.Context) { c.Set("user_id", 1) }, suite.handler.findUserByUserID())
+}
+
+// テストを実行するのに必要
+func TestUserHandlerSuite(t *testing.T) {
+	suite.Run(t, new(UserHandlerTestSuite))
+}
+
+func (suite *UserHandlerTestSuite) Test_userHandler_findUserByUserID_200() {
+	suite.mock.EXPECT().FindUserByUserID(gomock.Any(), 1).Return(
+		&model.User{
+			ID:       1,
+			Name:     "name1",
+			Icon:     "/icon1",
+			Gender:   1,
+			BirthDay: time.Date(2022, 1, 1, 0, 0, 0, 0, time.Local),
+			Location: 1,
 		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			gin.SetMode(gin.TestMode)
-			// mock登録
-			controller := gomock.NewController(t)
-			defer controller.Finish()
-			mock := mock.NewMockIUserUsecase(controller)
-			tt.prepareMockFn(mock)
-			// user handler 初期化(内部にmock)
-			uh := NewUserHandler(mock)
+		nil,
+	)
 
-			// エンドポイントを登録
-			// router := InitRouter()
-			router := gin.Default()
-			// ハンドラー登録(mockが登録させたハンドラー)
-			router.GET(usersAPIRoot+"/:user_id", func(c *gin.Context) { c.Set("user_id", 1) }, uh.findUserByUserID())
-
-			// レスポンスを受け止める*httptest.ResponseRecorder
-			rec := httptest.NewRecorder()
-			// テストで送信するリクエスト
-			req := httptest.NewRequest(http.MethodGet, usersAPIRoot+"/1", nil)
-			// リクエスト送信
-			router.ServeHTTP(rec, req)
-
-			// 結果確認
-			assert.Equal(t, http.StatusOK, rec.Code)
-			assert.JSONEq(
-				t,
-				`{
-					"id":1,
-					"name":"name1",
-					"icon":"/icon1",
-					"gender":1,
-					"birthday":"2022-01-01T00:00:00+09:00",
-					"location":1
-				}`,
-				rec.Body.String(),
-			)
-		})
-	}
+	// レスポンスを受け止める*httptest.ResponseRecorder
+	rec := httptest.NewRecorder()
+	// テストで送信するリクエスト
+	req := httptest.NewRequest(http.MethodGet, usersAPIRoot+"/1", nil)
+	// リクエスト送信
+	suite.router.ServeHTTP(rec, req)
+	// 結果確認
+	suite.Equal(http.StatusOK, rec.Code)
+	suite.JSONEq(
+		`{
+			"id":1,
+			"name":"name1",
+			"icon":"/icon1",
+			"gender":1,
+			"birthday":"2022-01-01T00:00:00+09:00",
+			"location":1
+		}`,
+		rec.Body.String(),
+	)
 }
 
 // func Test_userHandler_findUsers(t *testing.T) {
