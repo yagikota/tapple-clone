1ebc640690de24a8cde98000d60f1458cd0b83ed
yagikota <mdryzk64smsh@gmail.com>
2022-08-16 18:04:44 +0900

    add: handlerテスト追加(一旦完了)

--- pkg/adaptor/http/user_handler_test.go
+++ pkg/adaptor/http/user_handler_test.go
@@ -1,6 +1,8 @@
 package http
 
 import (
+	"bytes"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -16,42 +18,46 @@ import (
 // テストデータ
 var (
 	userID = 1
-
-	user11 = &model.User{
-		ID:       1,
+	user1  = &model.User{
+		ID:       model.UserID(userID),
 		Name:     "name1",
 		Icon:     "/icon1",
 		Gender:   1,
 		BirthDay: time.Date(2022, 1, 1, 0, 0, 0, 0, time.Local),
 		Location: 1,
 	}
-	user12 = &model.User{
-		ID:       2,
-		Name:     "name2",
-		Icon:     "/icon2",
-		Gender:   2,
-		BirthDay: time.Date(2022, 2, 2, 0, 0, 0, 0, time.Local),
-		Location: 2,
-	}
-	users1 = model.UserSlice{user11, user12}
+	userSlice1 = model.UserSlice{user1}
 
-	message11 = &model.Message{
+	message1 = &model.Message{
 		ID:        1,
 		UserID:    1,
 		Content:   "content1",
 		CreatedAt: time.Date(2022, 1, 1, 0, 0, 0, 0, time.Local),
 	}
-	messages1 = model.MessageSlice{message11}
+	messageSlice1 = model.MessageSlice{message1}
 
-	room1 = &model.Room{
-		ID:            1,
+	roomID = 1
+	room1  = &model.Room{
+		ID:            model.RoomID(roomID),
 		Unread:        1,
 		IsPinned:      true,
 		Name:          "name1",
 		Icon:          "/icon1",
-		LatestMessage: message11,
+		LatestMessage: message1,
 	}
-	rooms = model.RoomSlice{room1}
+	roomSlice  = model.RoomSlice{room1}
+	rooms1     = &model.Rooms{Rooms: roomSlice}
+	roomDetail = &model.RoomDetail{
+		ID:       model.RoomID(roomID),
+		Name:     "name1",
+		Icon:     "/icon1",
+		Users:    userSlice1,
+		Messages: messageSlice1,
+	}
+
+	newMessage1 = &model.NewMessage{
+		Content: "content1",
+	}
 )
 
 //  1.SetupSuite
@@ -81,13 +87,27 @@ func (suite *UserHandlerTestSuite) SetupSuite() {
 	suite.mock = mock.NewMockIUserUsecase(mockCtl)
 	suite.handler = NewUserHandler(suite.mock)
 
+	suite.router.Use(
+		func(ctx *gin.Context) {
+			ctx.Set("user_id", userID)
+			ctx.Set("room_id", roomID)
+		},
+	)
 	// ハンドラー登録
 	// v1/users
 	suite.router.GET(usersAPIRoot, suite.handler.findUsers())
 	// v1/users/{user_id}
-	suite.router.GET(usersAPIRoot+roomIDParam, func(c *gin.Context) { c.Set("user_id", 1) }, suite.handler.findUserByUserID())
+	path := usersAPIRoot + fmt.Sprintf("/:%s", userIDParam)
+	suite.router.GET(path, suite.handler.findUserByUserID())
 	// v1/users/{user_id}/rooms
-	suite.router.GET(usersAPIRoot+"/rooms", suite.handler.findRooms())
+	path = usersAPIRoot + fmt.Sprintf("/:%s/rooms", userIDParam)
+	suite.router.GET(path, suite.handler.findRooms())
+	// v1/users/{user_id}/rooms/{room_id}
+	path = usersAPIRoot + fmt.Sprintf("/:%s/rooms/:%s", userIDParam, roomIDParam)
+	suite.router.GET(path, suite.handler.findRoomDetailByRoomID())
+	// v1/users/{user_id}/rooms/{room_id}/messages
+	path = usersAPIRoot + fmt.Sprintf("/:%s/rooms/:%s/messages", userIDParam, roomIDParam)
+	suite.router.POST(path, suite.handler.sendMessage())
 }
 
 func (suite *UserHandlerTestSuite) SetupTest() {
@@ -100,7 +120,7 @@ func TestUserHandlerSuite(t *testing.T) {
 }
 
 func (suite *UserHandlerTestSuite) Test_userHandler_findUserByUserID_200() {
-	suite.mock.EXPECT().FindUserByUserID(gomock.Any(), userID).Return(user11, nil)
+	suite.mock.EXPECT().FindUserByUserID(gomock.Any(), userID).Return(user1, nil)
 	// レスポンスを受け止める*httptest.ResponseRecorder
 	rec := suite.rec
 	// テストで送信するリクエスト
@@ -123,118 +143,101 @@ func (suite *UserHandlerTestSuite) Test_userHandler_findUserByUserID_200() {
 }
 
 func (suite *UserHandlerTestSuite) Test_userHandler_findUsers_200() {
-	suite.mock.EXPECT().FindAllUsers(gomock.Any()).Return(users1, nil)
+	suite.mock.EXPECT().FindAllUsers(gomock.Any()).Return(userSlice1, nil)
 	rec := suite.rec
 	req := httptest.NewRequest(http.MethodGet, usersAPIRoot, nil)
 	suite.router.ServeHTTP(rec, req)
 	suite.Equal(http.StatusOK, rec.Code)
 	suite.JSONEq(
-		`
-		[
+		`[
 			{
 				"id":1,
 				"name":"name1",
 				"icon":"/icon1",
 				"gender":1,
 				"birthday":"2022-01-01T00:00:00+09:00",
-			"location":1
-			},
-			{
-				"id":2,
-				"name":"name2",
-				"icon":"/icon2",
-				"gender":2,
-				"birthday":"2022-02-02T00:00:00+09:00",
-				"location":2
+				"location":1
 			}
 		]`,
 		rec.Body.String(),
 	)
 }
 
-// func (suite *UserHandlerTestSuite) Test_userHandler_findRooms() {
-// 	suite.mock.EXPECT().FindAllRooms(gomock.Any(), userID).Return(rooms, nil)
-// 	rec := suite.rec
-// 	req := httptest.NewRequest(http.MethodGet, usersAPIRoot+"/rooms", nil)
-// 	suite.router.ServeHTTP(rec, req)
-// 	suite.Equal(http.StatusOK, rec.Code)
-// 	suite.JSONEq(
-// 		`
-// 		[
-// 			{
-// 				"id": 1,
-// 				"is_pinned": true,
-// 				"name": "name1",
-// 				"icon": "/icon1",
-// 				"latest_message": {
-// 					"id": 1,
-// 					"user_id": 1,
-// 					"content": "content1",
-// 					"is_read": true,
-// 					"created_at": "2022-01-01T00:00:00+09:00",
-// 				}
-// 			},
-// 			{
-// 				"id": 4,
-// 				"is_pinned": true,
-// 				"name": "チサキ",
-// 				"icon": "female/n000284/main_0001_02.jpg",
-// 				"latest_message": {
-// 					"id": 12,
-// 					"user_id": 4,
-// 					"content": "いつ行きましょうか！",
-// 					"is_read": false,
-// 					"created_at": "2022-07-08T00:00:00+09:00",
-// 				}
-// 			}
-// 		]
-// 		`,
-// 		rec.Body.String(),
-// 	)
-// }
+func (suite *UserHandlerTestSuite) Test_userHandler_findRooms_200() {
+	suite.mock.EXPECT().FindAllRooms(gomock.Any(), userID).Return(rooms1, nil)
+	rec := suite.rec
+	path := fmt.Sprintf("%s/%d/rooms", usersAPIRoot, userID)
+	req := httptest.NewRequest(http.MethodGet, path, nil)
+	suite.router.ServeHTTP(rec, req)
+	suite.Equal(http.StatusOK, rec.Code)
+	suite.JSONEq(
+		`{
+			"rooms": [
+				{
+					"id": 1,
+					"unread": 1,
+					"is_pinned": true,
+					"name": "name1",
+					"icon": "/icon1",
+					"latest_message": {
+						"id": 1,
+						"user_id": 1,
+						"content": "content1",
+						"created_at": "2022-01-01T00:00:00+09:00"
+					}
+				}
+			]
+		}`,
+		rec.Body.String(),
+	)
+}
 
-// func Test_userHandler_findRoomDetailByRoomID(t *testing.T) {
-// 	type fields struct {
-// 		uUsecase usecase.IUserUsecase
-// 	}
-// 	tests := []struct {
-// 		name   string
-// 		fields fields
-// 		want   gin.HandlerFunc
-// 	}{
-// 		// TODO: Add test cases.
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			uh := &userHandler{
-// 				uUsecase: tt.fields.uUsecase,
-// 			}
-// 			if got := uh.findRoomDetailByRoomID(); !reflect.DeepEqual(got, tt.want) {
-// 				t.Errorf("userHandler.findRoomDetailByRoomID() = %v, want %v", got, tt.want)
-// 			}
-// 		})
-// 	}
-// }
+func (suite *UserHandlerTestSuite) Test_userHandler_findRoomDetailByRoomID() {
+	suite.mock.EXPECT().FindRoomDetailByRoomID(gomock.Any(), userID, roomID).Return(roomDetail, nil)
+	rec := suite.rec
+	path := fmt.Sprintf("%s/%d/rooms/%d", usersAPIRoot, userID, roomID)
+	req := httptest.NewRequest(http.MethodGet, path, nil)
+	suite.router.ServeHTTP(rec, req)
+	suite.Equal(http.StatusOK, rec.Code)
+	suite.JSONEq(
+		`{
+			"id": 1,
+			"name": "name1",
+			"icon": "/icon1",
+			"users": [
+				{
+					"id": 1,
+					"name": "name1",
+					"icon": "/icon1",
+					"gender": 1,
+					"birthday": "2022-01-01T00:00:00+09:00",
+					"location": 1
+				}
+			],
+			"messages": [
+				{
+					"id": 1,
+					"user_id": 1,
+					"content": "content1",
+					"created_at": "2022-01-01T00:00:00+09:00"
+				}
+			]
+		}`,
+		rec.Body.String(),
+	)
+}
 
-// func Test_userHandler_sendMessage(t *testing.T) {
-// 	type fields struct {
-// 		uUsecase usecase.IUserUsecase
-// 	}
-// 	tests := []struct {
-// 		name   string
-// 		fields fields
-// 		want   gin.HandlerFunc
-// 	}{
-// 		// TODO: Add test cases.
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			uh := &userHandler{
-// 				uUsecase: tt.fields.uUsecase,
-// 			}
-// 			if got := uh.sendMessage(); !reflect.DeepEqual(got, tt.want) {
-// 				t.Errorf("userHandler.sendMessage() = %v, want %v", got, tt.want)
-// 			}
-// 		})
-// 	}
-// }
+func (suite *UserHandlerTestSuite) Test_userHandler_sendMessage_200() {
+	suite.mock.EXPECT().SendMessage(gomock.Any(), userID, roomID, newMessage1).Return(nil)
+	rec := suite.rec
+	path := fmt.Sprintf("%s/%d/rooms/%d/messages", usersAPIRoot, userID, roomID)
+	jsonStr := []byte(
+		`{
+			"content": "content1"
+		}`,
+	)
+	req := httptest.NewRequest(http.MethodPost, path, bytes.NewBuffer(jsonStr))
+	suite.router.ServeHTTP(rec, req)
+	suite.Equal(http.StatusOK, rec.Code)
+	suite.JSONEq("null", rec.Body.String())
+}
