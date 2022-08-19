9115a40976d43762dfbb0e59e38315b1c5489019
中田誠人 <masato.nakata1250softbank.com@gmail.com>
2022-08-20 03:55:36 +0900

    feat: hobby構造体定義を追加

---
+++ pkg/usecase/model/hobby.go
@@ -0,0 +1,19 @@
+package model
+
+import "github.com/CyberAgentHack/2208-ace-go-server/pkg/domain/model"
+
+type HobbyID int
+
+type HobbySlice []*Hobby
+
+type Hobby struct {
+	ID  HobbyID `json:"id"`
+	Tag string  `json:"tag"`
+}
+
+func HobbyFromDomainEntity(m *model.Hobby) *Hobby {
+	return &Hobby{
+		ID:  HobbyID(m.ID),
+		Tag: m.Tag,
+	}
+}
