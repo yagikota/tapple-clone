ff9d932ee471e60e2b4acee2e0b3290e2ecc0439
中田誠人 <masato.nakata1250softbank.com@gmail.com>
2022-08-20 03:39:40 +0900

    feat: sqlBolierによる生成&変更

--- pkg/domain/model/users.go
+++ pkg/domain/model/users.go
@@ -116,10 +116,12 @@ var UserWhere = struct {
 
 // UserRels is where relationship names are stored.
 var UserRels = struct {
+	Hobbies           string
 	Messages          string
 	RoomUsers         string
 	UserProfileImages string
 }{
+	Hobbies:           "Hobbies",
 	Messages:          "Messages",
 	RoomUsers:         "RoomUsers",
 	UserProfileImages: "UserProfileImages",
@@ -127,6 +129,7 @@ var UserRels = struct {
 
 // userR is where relationships are stored.
 type userR struct {
+	Hobbies           HobbySlice            `boil:"Hobbies" json:"Hobbies" toml:"Hobbies" yaml:"Hobbies"`
 	Messages          MessageSlice          `boil:"Messages" json:"Messages" toml:"Messages" yaml:"Messages"`
 	RoomUsers         RoomUserSlice         `boil:"RoomUsers" json:"RoomUsers" toml:"RoomUsers" yaml:"RoomUsers"`
 	UserProfileImages UserProfileImageSlice `boil:"UserProfileImages" json:"UserProfileImages" toml:"UserProfileImages" yaml:"UserProfileImages"`
@@ -135,6 +138,13 @@ type userR struct {
 // NewStruct creates a new relationship struct
 func (*userR) NewStruct() *userR {
 	return &userR{}
+}
+
+func (r *userR) GetHobbies() HobbySlice {
+	if r == nil {
+		return nil
+	}
+	return r.Hobbies
 }
 
 func (r *userR) GetMessages() MessageSlice {
@@ -467,6 +477,20 @@ func (q userQuery) Exists(ctx context.Context, exec boil.ContextExecutor) (bool,
 	return count > 0, nil
 }
 
+// Hobbies retrieves all the hobby's Hobbies with an executor.
+func (o *User) Hobbies(mods ...qm.QueryMod) hobbyQuery {
+	var queryMods []qm.QueryMod
+	if len(mods) != 0 {
+		queryMods = append(queryMods, mods...)
+	}
+
+	queryMods = append(queryMods,
+		qm.Where("`hobbies`.`user_id`=?", o.ID),
+	)
+
+	return Hobbies(queryMods...)
+}
+
 // Messages retrieves all the message's Messages with an executor.
 func (o *User) Messages(mods ...qm.QueryMod) messageQuery {
 	var queryMods []qm.QueryMod
@@ -507,6 +531,120 @@ func (o *User) UserProfileImages(mods ...qm.QueryMod) userProfileImageQuery {
 	)
 
 	return UserProfileImages(queryMods...)
+}
+
+// LoadHobbies allows an eager lookup of values, cached into the
+// loaded structs of the objects. This is for a 1-M or N-M relationship.
+func (userL) LoadHobbies(ctx context.Context, e boil.ContextExecutor, singular bool, maybeUser interface{}, mods queries.Applicator) error {
+	var slice []*User
+	var object *User
+
+	if singular {
+		var ok bool
+		object, ok = maybeUser.(*User)
+		if !ok {
+			object = new(User)
+			ok = queries.SetFromEmbeddedStruct(&object, &maybeUser)
+			if !ok {
+				return errors.New(fmt.Sprintf("failed to set %T from embedded struct %T", object, maybeUser))
+			}
+		}
+	} else {
+		s, ok := maybeUser.(*[]*User)
+		if ok {
+			slice = *s
+		} else {
+			ok = queries.SetFromEmbeddedStruct(&slice, maybeUser)
+			if !ok {
+				return errors.New(fmt.Sprintf("failed to set %T from embedded struct %T", slice, maybeUser))
+			}
+		}
+	}
+
+	args := make([]interface{}, 0, 1)
+	if singular {
+		if object.R == nil {
+			object.R = &userR{}
+		}
+		args = append(args, object.ID)
+	} else {
+	Outer:
+		for _, obj := range slice {
+			if obj.R == nil {
+				obj.R = &userR{}
+			}
+
+			for _, a := range args {
+				if a == obj.ID {
+					continue Outer
+				}
+			}
+
+			args = append(args, obj.ID)
+		}
+	}
+
+	if len(args) == 0 {
+		return nil
+	}
+
+	query := NewQuery(
+		qm.From(`hobbies`),
+		qm.WhereIn(`hobbies.user_id in ?`, args...),
+	)
+	if mods != nil {
+		mods.Apply(query)
+	}
+
+	results, err := query.QueryContext(ctx, e)
+	if err != nil {
+		return errors.Wrap(err, "failed to eager load hobbies")
+	}
+
+	var resultSlice []*Hobby
+	if err = queries.Bind(results, &resultSlice); err != nil {
+		return errors.Wrap(err, "failed to bind eager loaded slice hobbies")
+	}
+
+	if err = results.Close(); err != nil {
+		return errors.Wrap(err, "failed to close results in eager load on hobbies")
+	}
+	if err = results.Err(); err != nil {
+		return errors.Wrap(err, "error occurred during iteration of eager loaded relations for hobbies")
+	}
+
+	if len(hobbyAfterSelectHooks) != 0 {
+		for _, obj := range resultSlice {
+			if err := obj.doAfterSelectHooks(ctx, e); err != nil {
+				return err
+			}
+		}
+	}
+	if singular {
+		object.R.Hobbies = resultSlice
+		for _, foreign := range resultSlice {
+			if foreign.R == nil {
+				foreign.R = &hobbyR{}
+			}
+			foreign.R.User = object
+		}
+		return nil
+	}
+
+	for _, foreign := range resultSlice {
+		for _, local := range slice {
+			if local.ID == foreign.UserID {
+				local.R.Hobbies = append(local.R.Hobbies, foreign)
+				if foreign.R == nil {
+					foreign.R = &hobbyR{}
+				}
+				foreign.R.User = local
+				break
+			}
+		}
+	}
+
+	return nil
 }
 
 // LoadMessages allows an eager lookup of values, cached into the
@@ -848,6 +986,68 @@ func (userL) LoadUserProfileImages(ctx context.Context, e boil.ContextExecutor, 
 		}
 	}
 
+	return nil
+}
+
+// AddHobbiesG adds the given related objects to the existing relationships
+// of the user, optionally inserting them as new records.
+// Appends related to o.R.Hobbies.
+// Sets related.R.User appropriately.
+// Uses the global database handle.
+func (o *User) AddHobbiesG(ctx context.Context, insert bool, related ...*Hobby) error {
+	return o.AddHobbies(ctx, boil.GetContextDB(), insert, related...)
+}
+
+// AddHobbies adds the given related objects to the existing relationships
+// of the user, optionally inserting them as new records.
+// Appends related to o.R.Hobbies.
+// Sets related.R.User appropriately.
+func (o *User) AddHobbies(ctx context.Context, exec boil.ContextExecutor, insert bool, related ...*Hobby) error {
+	var err error
+	for _, rel := range related {
+		if insert {
+			rel.UserID = o.ID
+			if err = rel.Insert(ctx, exec, boil.Infer()); err != nil {
+				return errors.Wrap(err, "failed to insert into foreign table")
+			}
+		} else {
+			updateQuery := fmt.Sprintf(
+				"UPDATE `hobbies` SET %s WHERE %s",
+				strmangle.SetParamNames("`", "`", 0, []string{"user_id"}),
+				strmangle.WhereClause("`", "`", 0, hobbyPrimaryKeyColumns),
+			)
+			values := []interface{}{o.ID, rel.ID}
+
+			if boil.IsDebug(ctx) {
+				writer := boil.DebugWriterFrom(ctx)
+				fmt.Fprintln(writer, updateQuery)
+				fmt.Fprintln(writer, values)
+			}
+			if _, err = exec.ExecContext(ctx, updateQuery, values...); err != nil {
+				return errors.Wrap(err, "failed to update foreign table")
+			}
+
+			rel.UserID = o.ID
+		}
+	}
+
+	if o.R == nil {
+		o.R = &userR{
+			Hobbies: related,
+		}
+	} else {
+		o.R.Hobbies = append(o.R.Hobbies, related...)
+	}
+
+	for _, rel := range related {
+		if rel.R == nil {
+			rel.R = &hobbyR{
+				User: o,
+			}
+		} else {
+			rel.R.User = o
+		}
+	}
 	return nil
 }
 
