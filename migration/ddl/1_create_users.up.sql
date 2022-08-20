6082652bb4242de4f2e74fbe46896f25409136d8
中田誠人 <masato.nakata1250softbank.com@gmail.com>
2022-08-20 17:06:39 +0900

    feat: usersテーブルにis_pinnedカラム追加

--- migration/ddl/1_create_users.up.sql
+++ migration/ddl/1_create_users.up.sql
@@ -6,6 +6,7 @@ CREATE TABLE
         gender INT(11) NOT NULL COMMENT "性別（男: 0, 女: 1）",
         birthday DATE NOT NULL COMMENT "誕生日",
         location INT(11) NOT NULL COMMENT "所在地(0: その他, 北海道:1~沖縄: 47)",
+        is_pinned BOOLEAN NOT NULL DEFAULT 0 COMMENT "本人認証(0: されていない, 1: されている)",
         created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT "ユーザー作成日時",
         updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "ユーザー更新日時",
         deteled_at DATETIME COMMENT "ユーザー論理削除日時",
